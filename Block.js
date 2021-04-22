5d34aa4369e972eb3ff3d45f93852d664731be3c
Affanali631 <75484768+Affanali631@users.noreply.github.com>
2021-04-22 10:28:33 +0530

    Update Block.js

--- Block.js
+++ Block.js
@@ -22,8 +22,7 @@ class Block{
     World.remove(world,this.body)
     push()
     this.visibility=this.visibility-10
-    tint(255,this.visibility)
-    rect(pos.x,pos.y,this.width, this.height);
+   
    pop()
   }
 }
